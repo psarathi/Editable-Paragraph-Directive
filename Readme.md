99e9baf754d64e344c26f7d2b2c9d991a24da251
psarathi <parthasarathi@gmail.com>
2014-03-22 19:58:45 -0700

    Changes to make it generic
    Instead of a paragraph, now the element to be made editable can be
    provided as an attribute

--- Readme.md
+++ Readme.md
@@ -1,7 +1,7 @@
-##Editable paragraph as an AngularJS directive##
+##Editable element as an AngularJS directive##
 
-This directive basically becomes an editable paragraph when the user clicks on it. It goes
-back to read-only state once the user click outside the paragraph.
+This directive basically becomes an editable element when the user clicks on it. It goes
+back to read-only state once the user click outside the element.
 
 Plunk:
 http://plnkr.co/edit/j979NcErHhVmqEoxFm1J
--- app.js
+++ app.js
@@ -1,53 +1,129 @@
 var app = angular.module('plunker', []);
 
 app.controller('MainCtrl', function($scope) {
-  $scope.contentCaption = 'World';
-  $scope.editMode = false;
-  $scope.startEdit = function(){
-    
-   $scope.editMode = true;
+    $scope.h2Content = 'This is h2';
+    $scope.spanContent = 'This is span';
+    $scope.pContent = 'This is paragraph';
+    $scope.editMode = false;
+    $scope.startEdit = function() {
+
+        $scope.editMode = true;
 
 
-  };
-  
-  $scope.$watch('contentCaption', function(nv, ov){
-    if(!angular.equals(nv,ov)){
-      console.info('Update service called');
-    }
-  });
-  
-    $scope.toggleEditMode = function(){
-  $scope.editMode = !$scope.editMode;
-  };
+    };
+
+    $scope.$watch('h2Content', function(nv, ov) {
+        if (!angular.equals(nv, ov)) {
+            console.info('Update service called');
+        }
+    });
+
+    $scope.$watch('spanContent', function(nv, ov) {
+        if (!angular.equals(nv, ov)) {
+            console.info('Update service called');
+        }
+    });
+
+    $scope.$watch('pContent', function(nv, ov) {
+        if (!angular.equals(nv, ov)) {
+            console.info('Update service called');
+        }
+    });
+
+
+    $scope.toggleEditMode = function() {
+        $scope.editMode = !$scope.editMode;
+    };
 
 });
 
-app.directive('editablePara', function($document){
-  var linkFunction = function(scope, element, attributes){
-    // console.log('document: ' + $document);
-    // console.log(scope);
-      $document.bind('click', function(e){
-      // console.log(e.target);
-      // console.log(element[0]);
-      if(e.target === element[0]){
-        // console.log('P click event: setting editMode to true');
-      scope.editMode = true;
-      }else{
-        // console.log('Body click event: setting editMode to false');
+app.directive('editablePara', function($document, $compile, $parse) {
+    var getTemplate = function(tag, id) {
+        tag = tag || 'p';
+
+        return '<' + tag + ' class="editable" id="' + id + '" ng-class="{isEditing:editMode}" contenteditable="{{editMode}}">{{content}}</' + tag + '>'
+    };
+
+    var linkFunction = function(scope, element, attributes) {
+        // console.log('document: ' + $document);
+        // console.log(scope.tag);
         scope.editMode = false;
-        scope.contentCaption = element.text();
-      }
-      // console.log(scope);
-      scope.$apply();
-      e.stopPropagation();
-    });    
-  };
-  
-  return {
-    link: linkFunction,
-    restrict: 'E',
-    replace: true,
-    transclude: true,
-    template: '<p id="editable" ng-class="{isEditing:editMode}" contenteditable="{{editMode}}" ng-model="content">{{contentCaption}}</p>'
-  }
+        element.html(getTemplate(attributes.tag, attributes.tagId));
+
+        $compile(element.contents())(scope);
+
+
+        $document.bind('click', function(e) {
+            // console.log(scope.editMode);
+            // console.log(e.target);
+            // console.log(element.children()[0]);
+            if (e.target === element.children()[0]) {
+                // console.log('P click event: setting editMode to true');
+                scope.editMode = true;
+            } else {
+                // console.log('Body click event: setting editMode to false');
+                scope.editMode = false;
+                scope.content = element.children().text();
+            }
+            // console.log(scope);
+            scope.$apply();
+            e.stopPropagation();
+        });
+    };
+
+    var compileFunction = function(element, attributes) {
+        var html = getTemplate(attributes.tag, attributes.tagId);
+        var modelAccessor = $parse(attributes.content);
+
+        var newElem = angular.element(html);
+        element.replaceWith(newElem);
+
+        return function(scope, element, attributes) {
+            // console.log('document: ' + $document);
+            // console.log(scope.tag);
+            scope.editMode = false;
+            // element.html(getTemplate(attributes.tag));
+
+            $compile(element)(scope);
+
+            element.bind('keydown', function(e) {
+                // console.log(e.keyCode);
+                if (e.keyCode == 13) {
+                    e.preventDefault();
+                }
+            });
+
+            $document.bind('click', function(e) {
+                // console.log(scope.editMode);
+                // console.log(e.target);
+                // console.log(element.children()[0]);
+                // console.log(element);
+                if (e.target === element[0]) {
+                    // console.log('P click event: setting editMode to true');
+                    scope.editMode = true;
+                } else {
+                    // console.log('Body click event: setting editMode to false');
+                    scope.editMode = false;
+                    // scope.content = element.text();
+                    // console.log(element.html());
+                    scope.$apply(function(scope) {
+                        modelAccessor.assign(scope.$parent, element.text());
+                    });
+                }
+                // console.log(scope);
+                // scope.$apply();
+                e.stopPropagation();
+            });
+        };
+    };
+
+    return {
+        scope: {
+            content: '='
+        },
+        // link: linkFunction,
+        compile: compileFunction,
+        restrict: 'E',
+        replace: true
+    }
 });
