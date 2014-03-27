90702b0bca8422a82e8c42a0dfe4fac5ccb5c32b
psarathi <parthasarathi@gmail.com>
2014-03-26 22:37:31 -0700

    Code and clean up and added class option
    Cleaned up some code
    Added better comments
    Feature to add classes by specifying it under tag-class attribute

--- app.js
+++ app.js
@@ -13,21 +13,27 @@ app.controller('MainCtrl', function($scope) {
     };
 
     $scope.$watch('h2Content', function(nv, ov) {
-        if (!angular.equals(nv, ov)) {
-            console.info('Update service called');
+        if (angular.equals(nv, ov)) {
+            return;
         }
+        // Code to execute when the value changes
+        console.info('Update service called');
     });
 
     $scope.$watch('spanContent', function(nv, ov) {
-        if (!angular.equals(nv, ov)) {
-            console.info('Update service called');
+        if (angular.equals(nv, ov)) {
+            return;
         }
+        // Code to execute when the value changes
+        console.info('Update service called');
     });
 
     $scope.$watch('pContent', function(nv, ov) {
-        if (!angular.equals(nv, ov)) {
-            console.info('Update service called');
+        if (angular.equals(nv, ov)) {
+            return;
         }
+        // Code to execute when the value changes
+        console.info('Update service called');
     });
 
 
@@ -38,80 +44,43 @@ app.controller('MainCtrl', function($scope) {
 });
 
 app.directive('editablePara', function($document, $compile, $parse) {
-    var getTemplate = function(tag, id) {
+    var getTemplate = function(tag, id, tagClass) {
         tag = tag || 'p';
 
-        return '<' + tag + ' class="editable" id="' + id + '" ng-class="{isEditing:editMode}" contenteditable="{{editMode}}">{{content}}</' + tag + '>'
-    };
-
-    var linkFunction = function(scope, element, attributes) {
-        // console.log('document: ' + $document);
-        // console.log(scope.tag);
-        scope.editMode = false;
-        element.html(getTemplate(attributes.tag, attributes.tagId));
-
-        $compile(element.contents())(scope);
-
-
-        $document.bind('click', function(e) {
-            // console.log(scope.editMode);
-            // console.log(e.target);
-            // console.log(element.children()[0]);
-            if (e.target === element.children()[0]) {
-                // console.log('P click event: setting editMode to true');
-                scope.editMode = true;
-            } else {
-                // console.log('Body click event: setting editMode to false');
-                scope.editMode = false;
-                scope.content = element.children().text();
-            }
-            // console.log(scope);
-            scope.$apply();
-            e.stopPropagation();
-        });
+        return '<' + tag + ' class="editable ' + tagClass + '" id="' + id + '" ng-class="{isEditing:editMode}" contenteditable="{{editMode}}">{{content}}</' + tag + '>'
     };
 
     var compileFunction = function(element, attributes) {
-        var html = getTemplate(attributes.tag, attributes.tagId);
+        var html = getTemplate(attributes.tag, attributes.tagId, attributes.tagClass);
         var modelAccessor = $parse(attributes.content);
 
         var newElem = angular.element(html);
         element.replaceWith(newElem);
 
         return function(scope, element, attributes) {
-            // console.log('document: ' + $document);
-            // console.log(scope.tag);
             scope.editMode = false;
-            // element.html(getTemplate(attributes.tag));
-
             $compile(element)(scope);
 
+            // Prevent the user from entering enter
             element.bind('keydown', function(e) {
-                // console.log(e.keyCode);
                 if (e.keyCode == 13) {
                     e.preventDefault();
                 }
             });
 
             $document.bind('click', function(e) {
-                // console.log(scope.editMode);
-                // console.log(e.target);
-                // console.log(element.children()[0]);
-                // console.log(element);
+                // If the element was clicked, make it editable
                 if (e.target === element[0]) {
-                    // console.log('P click event: setting editMode to true');
-                    scope.editMode = true;
+                    scope.$apply(function(scope) {
+                        scope.editMode = true;
+                    });
                 } else {
-                    // console.log('Body click event: setting editMode to false');
+                    // If the click was outside the element, make it read-only and assign the updated value back to the parent scope
                     scope.editMode = false;
-                    // scope.content = element.text();
-                    // console.log(element.html());
                     scope.$apply(function(scope) {
                         modelAccessor.assign(scope.$parent, element.text());
                     });
                 }
-                // console.log(scope);
-                // scope.$apply();
                 e.stopPropagation();
             });
         };
@@ -121,7 +90,6 @@ app.directive('editablePara', function($document, $compile, $parse) {
         scope: {
             content: '='
         },
-        // link: linkFunction,
         compile: compileFunction,
         restrict: 'E',
         replace: true
